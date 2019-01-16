e846308c01095e622d47e3f1d8b1258ffef381c4
marcus <marcusmmourao@gmail.com>
2019-01-16 21:58:01 -0200

    Rename files and update tests

---
+++ test/fixtures/api/weather-api-fixtures.js
@@ -0,0 +1,11 @@
+const fixtures = {
+  apiKey: 'c0cef6453abf7c54165726c03fe60665',
+  serverHost: 'https://api.openweathermap.org/data/2.5/weather',
+  cityId: 123,
+};
+
+fixtures.getWeatherByCityId = {
+  finalEndPoint: `${fixtures.serverHost}?id=${fixtures.cityId}&appid=${fixtures.apiKey}`
+};
+
+export default fixtures;
--- test/unit/api/WeatherAPI.spec.js
+++ test/unit/api/WeatherAPI.spec.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 import {stub} from 'sinon';
-import WeatherAPI from '../../../api/weathernow'
+import WeatherAPI from '../../../api/WeatherAPI'
+import fixtures from '../../fixtures/api/weather-api-fixtures';
 
 describe('Unit tests for Weather Now API', () => {
   it('Test if function getWeatherAPIKey return expected value', () => {
-    expect(WeatherAPI.getWeatherAPIKey()).to.equal('c0cef6453abf7c54165726c03fe60665');
+    expect(WeatherAPI.getWeatherAPIKey()).to.equal(fixtures.apiKey);
   });
   it('Test if function getWeatherServerHost return expected value', () => {
-    expect(WeatherAPI.getWeatherServerHost()).to.equal('https://api.openweathermap.org/data/2.5/weather');
+    expect(WeatherAPI.getWeatherServerHost()).to.equal(fixtures.serverHost);
   });
   it('Test if function getWeatherInfo calls axios with correct parameter', () => {
     const stubAxiosGet = stub(axios, 'get');
-    const cityId = 123;
-    const expectedArg = 'https://api.openweathermap.org/data/2.5/weather?id=123&appid=c0cef6453abf7c54165726c03fe60665';
+    const cityId = fixtures.cityId;
+    const expectedArg = fixtures.getWeatherByCityId.finalEndPoint;
     WeatherAPI.getWeatherInfoByCityId(cityId);
     expect(stubAxiosGet.calledOnce).to.equal(true);
     expect(stubAxiosGet.getCall(0).args[0]).to.equal(expectedArg)
-
   });
 });
