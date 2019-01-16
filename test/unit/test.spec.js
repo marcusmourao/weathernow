e5468a0d812301576015946a3ac8b609d2076d25
marcus <marcusmmourao@gmail.com>
2019-01-16 20:23:28 -0200

    Delete template stuff

--- test/unit/test.spec.js
+++ test/unit/test.spec.js
@@ -1,14 +1,3 @@
-import {shallowMount} from '@vue/test-utils'
-import component from '../../components/Logo.vue';
 
 describe('Unit test test', () => {
-  const wrapper = shallowMount(component);
-  it('Test if tests are working', () => {
-    expect(true).to.equal(true);
-    expect(wrapper.find('p').exists()).to.equal(true);
-    expect(wrapper.find('p').text()).to.equal('Hello World');
-  });
-  it('Test if component has correct data', () => {
-    expect(wrapper.vm.state).to.equal('state fulsl');
-  });
 });
