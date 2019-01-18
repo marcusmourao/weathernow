37f02d8c79f9caebb4c883254dd9ff7a69561069
marcus <marcusmmourao@gmail.com>
2019-01-17 23:01:08 -0200

    Remove debug log

--- helpers/date.js
+++ helpers/date.js
@@ -1,6 +1,5 @@
 class DateHelper {
   static getCurrentISODateString() {
-    console.log('dentro', process.env.NODE_ENV);
     if (process.env.NODE_ENV === 'none') return '2019-01-18T00:13:35.393Z';
     return new Date().toISOString();
   }
